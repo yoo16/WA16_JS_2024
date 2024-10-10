6095276789a697bcc6f5e33007d313c6dc2e12ae
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-10 16:39:30 +0900

    update

--- 02_variable/js/variable.js
+++ 02_variable/js/variable.js
@@ -9,5 +9,14 @@ document.getElementById("name").innerHTML = name
 name = "紅茶"
 document.getElementById("name").innerHTML = name
 
+// 価格
 var price = 500
 document.getElementById("price").innerHTML = price
+
+// 個数
+var quantity = 5
+document.getElementById("quantity").innerHTML = quantity
+
+// 割引 
+var discount = 100
+document.getElementById("discount").innerHTML = discount
