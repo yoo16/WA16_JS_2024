21ea63d28d93b9ceaa585c6777104ade5ea43434
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-10 16:24:35 +0900

    update

--- 02_variable/js/variable.js
+++ 02_variable/js/variable.js
@@ -1,3 +1,10 @@
+// id=name のHTMLタグを取得して、タグの中に文字列を入れる（表示）
 document.getElementById("name").innerHTML = "お茶"
 
 // 変数定義
+var name = "コーヒー";
+document.getElementById("name").innerHTML = name
+
+// 再代入
+name = "紅茶"
+document.getElementById("name").innerHTML = name
