8ff9f80259a04b2649904126467439d9ac39c5c5
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-10 16:28:45 +0900

    update

--- 02_variable/js/variable.js
+++ 02_variable/js/variable.js
@@ -2,9 +2,12 @@
 document.getElementById("name").innerHTML = "お茶"
 
 // 変数定義
-var name = "コーヒー";
+var name = "コーヒー"
 document.getElementById("name").innerHTML = name
 
 // 再代入
 name = "紅茶"
 document.getElementById("name").innerHTML = name
+
+var price = 500
+document.getElementById("price").innerHTML = price
