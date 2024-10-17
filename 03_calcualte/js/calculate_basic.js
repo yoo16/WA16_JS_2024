6f200f7ec7ebb565d9a7af4063429d7114675124
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-17 11:51:03 +0900

    update

--- 03_calcualte/js/calculate_basic.js
+++ 03_calcualte/js/calculate_basic.js
@@ -4,9 +4,11 @@ var discount = 100
 const TAX_RATE = 0.1
 var totalPrice = 0
 
+// 合計金額を計算
 totalPrice = (price * quantity - discount) * (1 + TAX_RATE)
 
 // 小数点を処理
 totalPrice = totalPrice.toFixed()
 
+// コンソール表示
 console.log(totalPrice)
