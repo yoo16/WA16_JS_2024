8370cdb34a71b8d04a0e4c6ca638401b2dce4f40
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-17 12:35:44 +0900

    updaet

--- 03_calcualte/js/calculate_app.js
+++ 03_calcualte/js/calculate_app.js
@@ -7,8 +7,16 @@ var discount = 100;
 var totalPrice = 0;
 
 // TODO: 個数を増減
+quantity++
+
 // TODO: 合計金額計算
+totalPrice = (price * quantity - discount) * (1 + TAX_RATE)
 // TODO: 四捨五入
+totalPrice = totalPrice.toFixed()
+
+// ブラウザのキャッシュ対策
+// Win: Ctrl + Shift + R
+// Mac: Cmd + Shift + R
 
 // HTML表示
 document.getElementById('name').innerHTML = name;
@@ -23,4 +31,12 @@ var tableNo = 3;
 var orderNo = 20341;
 
 // TODO: オーダーコード生成（テキスト連結）
+var orderCode = tableNo + "-" + orderNo
+
 // TODO: オーダーコードHTML表示
+var tableNo = 3;
+var orderNo = 20341;
+
+// TODO: オーダーコード生成（テキスト連結）
+var orderCode = tableNo + "-" + orderNo
+document.getElementById('order-code').innerHTML = orderCode
