5a79f182e3416ff43ebf6a0186679dc252a559d6
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-17 15:18:15 +0900

    update

--- 04_function/js/functions_basic.js
+++ 04_function/js/functions_basic.js
@@ -8,6 +8,20 @@ var number = parseInt(numberString)
 console.log(numberString)
 console.log(number)
 
+// 数字かどうか判別
+var result = isNaN("HAL")
+console.log(result)
+var result = isNaN(numberString)
+console.log(result)
+var result = isNaN(number)
+console.log(result)
+
+// 3秒後(3000ms）に文字を表示
+// sett
+setTimeout(() => {
+    console.log("実行しました！")
+}, 3000);
+
 /**
  * ユーザ定義関数
  */
