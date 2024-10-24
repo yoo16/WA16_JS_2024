79097f75c17c012a7ffe2ef38775848b8d4adbd6
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-24 11:40:53 +0900

    update

--- 05_condition/js/condition_basic.js
+++ 05_condition/js/condition_basic.js
@@ -12,3 +12,12 @@ console.log(result)
 
 result = (price >= 500)
 console.log(result)
+
+// If Statement
+var message = ""
+var price = 500
+var money = 1000
+
+if (money >= price) {
+    console.log("決済完了")
+}
