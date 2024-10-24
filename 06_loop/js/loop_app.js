6f6a758721feaca6d1fe3e63dc6de5c17f2d565f
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-24 16:33:14 +0900

    update

--- 06_loop/js/loop_app.js
+++ 06_loop/js/loop_app.js
@@ -4,17 +4,18 @@ const ageSelect = document.getElementById('age-select');
 const defaultYear = 30;
 
 // TODO: optionタグ生成の繰り返し
+// ageSelect という <select> タグを取得する
+
 for (let index = 0; index <= 100; index++) {
-    // --- ここから ---
     // optionタグ生成​
     const option = document.createElement('option');
-    option.value = 0;
-    option.innerHTML = 0 + ' 歳';
+    option.value = index; // index を value に設定
+    option.innerHTML = index + ' 歳'; // index + " 歳" を表示
 
     // selectタグ に optionタグ追加​
     ageSelect.appendChild(option);
-    // --- ここまで ---
 }
+
 
 /**
  * 複利計算 while
