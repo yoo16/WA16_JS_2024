1010243b80c4c1eba85d14485fd49acf4b73fca2
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-24 12:04:14 +0900

    update

--- 05_condition/js/condition_basic.js
+++ 05_condition/js/condition_basic.js
@@ -15,7 +15,7 @@ console.log(result)
 
 // If Statement
 var message = ""
-var price = -100
+var price = 500
 var money = 1000
 
 if (money >= price) {
@@ -30,3 +30,7 @@ if (price < 0) {
 } else {
     console.log("残高不足")
 }
+
+// 三項演算
+message = (price <= money) ? "決済完了" : "残高不足"
+console.log(message)
