5f6d6b51c5e6a23b2e0ded69fbb6c992ee9e1ef5
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-31 11:39:20 +0900

    update

--- wa16_homework_01/js/main.js
+++ wa16_homework_01/js/main.js
@@ -3,39 +3,52 @@
  * JSのプログラムで、自分の氏名と学籍番号と変数定義してHTML表示してください。
  */
 // TODO: 学籍番号と氏名の変数を定義
+var studentNumber = "0000001"
+var studentName = "HAL 太郎"
 
 // TODO: 学籍番号と氏名をHTMLに表示
-document.getElementById('studentNumber').innerHTML;
-document.getElementById('studentName').innerHTML;
+document.getElementById('studentNumber').innerHTML = studentNumber;
+document.getElementById('studentName').innerHTML = studentName;
 
 /**
  * documentオブジェクト
  * JSのプログラムで、ウィンドウタイトルを「課題01-学籍番号-氏名」に変更してください。
  */
 // TODO: ウィンドウタイトルを「課題01-学籍番号-氏名」に変更
+document.title = "課題01-" + studentNumber + "-" + studentName
 
 /**
  * 演算
  * JSのプログラムで、つぎの問題を完成させてください。
  */
 // TODO: 思い浮かんだ好きな数字を memoryNumber に代入
-var memoryNumber;
+var memoryNumber = 36;
 
 // TODO: 思い浮かんだ好きな数字をHTMLに表示
-document.getElementById('memoryNumber').innerHTML;
-
+document.getElementById('memoryNumber').innerHTML = memoryNumber;
 
 // TODO: 計算
 // 思い浮かんだ好きな数字を、答えに代入（初期値）
 var answerNumber = memoryNumber;
 
 // 1. その数字（answerNumber）に3をかけます。
+answerNumber *= 3
+// answerNumber = answerNumber * 3
+
 // 2. 結果に6を足します。
+answerNumber += 6
+// answerNumber = answerNumber + 6
+
 // 3. その結果を3で割ります。
+answerNumber /= 3
+// answerNumber = answerNumber / 3
+
 // 4. 最初に思い浮かべた数字を引いてください。
+answerNumber -= memoryNumber
+// answerNumber = answerNumber - memoryNumber
 
 // TODO: 答え「answerNumber」をHTML表示
-document.getElementById('answerNumber').innerHTML;
+document.getElementById('answerNumber').innerHTML = answerNumber;
 
 
 /**
