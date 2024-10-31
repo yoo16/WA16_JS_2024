8f7b751fc9f3d8eedf0fe6aeac502fdeada4ac7b
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-31 15:06:39 +0900

    update

--- 08_array/js/array_app.js
+++ 08_array/js/array_app.js
@@ -1,13 +1,14 @@
 // TODO: 配列の定義（'コーヒー', '紅茶', 'ほうじ茶'）
-var items
 var items = ['コーヒー', '紅茶', 'ほうじ茶'];
+
+console.log(items)
 
 /**
  * 配列を文字列で表示
  */
 function showArrayString() {
     // 配列をカンマ区切りの文字列に変換
-    var string = items.join(', ');
+    var string = items.join(',');
     document.getElementById('array-string').innerHTML = string;
 }
 
@@ -15,15 +16,17 @@ function showArrayString() {
  * 配列の要素表示
  */
 function showItem() {
+    // ブラウザで入力したインデックスを取得
     const index = document.getElementById('input-index').value;
+    console.log(index)
     // インデックス範囲内かチェック
     if (index >= 0 && index < items.length) {
         // TODO: items に index を指定してデータ表示
-        document.getElementById('input-value').value;
+        document.getElementById('input-value').value = items[index];
         document.getElementById('error').innerHTML = "";
     } else {
         // TODO: 配列の個数から、最大インデックスを設定
-        var maxIndex = items.length;
+        var maxIndex = items.length - 1;
         var error = `0 から${maxIndex}の範囲で指定してください。`;
         document.getElementById('error').innerHTML = error;
     }
