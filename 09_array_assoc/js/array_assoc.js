298ee8fd14ce7e7f22970342e37fd96ffb0b4c67
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-31 16:05:38 +0900

    update

--- 09_array_assoc/js/array_assoc.js
+++ 09_array_assoc/js/array_assoc.js
@@ -3,12 +3,21 @@ const furigana = document.getElementById('current-furigana');
 const next = document.getElementById('next-station');
 const prev = document.getElementById('prev-station');
 
-// 連想配列の定義
+// 連想配列（オブジェクト）の定義
 var station = {
-
+    name: "東京",
+    furigana: "とうきょう",
 }
+// DevTools で確認
+console.log(station)
 
-// 駅名表示
+// ブラウザの強制再読み込み
+// Windows: Ctrl + Shift + R
+// Mac: Cmd + Shift + R
+
+// TODO:駅名表示
+current.innerHTML = station.name
+furigana.innerHTML = station.furigana
 
 // 駅名、ふりがなのデータ更新
 
