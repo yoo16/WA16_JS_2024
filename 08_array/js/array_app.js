6b1ff01c43a8b624977d3cf22e770b97832ca9d8
liuchiawei <doublecheap@gmail.com>
2024-10-31 15:11:41 +0900

    update at 15:11

--- 08_array/js/array_app.js
+++ 08_array/js/array_app.js
@@ -1,13 +1,14 @@
 // TODO: 配列の定義（'コーヒー', '紅茶', 'ほうじ茶'）
-var items
-var items = ['コーヒー', '紅茶', 'ほうじ茶'];
+var drinks = ['コーヒー', '紅茶', 'ほうじ茶'];
+console.log(drinks);
 
 /**
  * 配列を文字列で表示
  */
 function showArrayString() {
     // 配列をカンマ区切りの文字列に変換
-    var string = items.join(', ');
+    // join() メソッドは、配列の要素を指定した文字列で結合して、新しい文字列を返す
+    var string = drinks.join(', ');
     document.getElementById('array-string').innerHTML = string;
 }
 
@@ -17,13 +18,13 @@ function showArrayString() {
 function showItem() {
     const index = document.getElementById('input-index').value;
     // インデックス範囲内かチェック
-    if (index >= 0 && index < items.length) {
+    if (index >= 0 && index < drinks.length) {
         // TODO: items に index を指定してデータ表示
-        document.getElementById('input-value').value;
+        document.getElementById('input-value').value = drinks[index];
         document.getElementById('error').innerHTML = "";
     } else {
         // TODO: 配列の個数から、最大インデックスを設定
-        var maxIndex = items.length;
+        var maxIndex = drinks.length - 1;
         var error = `0 から${maxIndex}の範囲で指定してください。`;
         document.getElementById('error').innerHTML = error;
     }
