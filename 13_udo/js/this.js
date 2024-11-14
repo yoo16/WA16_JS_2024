d3f9aee713d160922c4ce92abdf24c915ea8ac5d
yoo16 <yohei.yoshikawa@gmail.com>
2024-11-14 11:44:36 +0900

    update

--- 13_udo/js/this.js
+++ 13_udo/js/this.js
@@ -10,18 +10,21 @@ document.getElementById('window-userAgent').innerText
     = navigator.userAgent
 
 // TODO: ウィンドウサイズ
-document.getElementById('window-size').innerText;
+document.getElementById('window-size').innerText
+    = screen.width + " x " + screen.height
 
 function clickEvent(element, event) {
     // Element取得
     console.log(element);
 
     // TODO: Elementからクラス名表示
-    document.getElementById('this-object').innerText;
+    document.getElementById('this-object').innerText
+        = element.className
 
     // イベント取得
     console.log(event);
 
     // TODO: イベントのタイプを表示
-    document.getElementById('event-object').innerText;
+    document.getElementById('event-object').innerText
+        = event.type
 }
