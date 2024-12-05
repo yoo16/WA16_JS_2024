8be12ddc644bca2127145715a35b08fdcf066ef6
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-05 14:38:29 +0900

    udpate

---
+++ 21_jquery_3/js/box.js
@@ -0,0 +1,20 @@
+$(function () {
+    const box1 = $("#box1");
+
+    // TODO: 各種情報の取得
+    const width = 0
+    const height = 0
+    const offset = 0
+    const id = "";
+
+    // text() を使って値を設定
+    $("#id-info span").text(id);
+    $("#width-info span").text(`${width}px`);
+    $("#height-info span").text(`${height}px`);
+    $("#offset-info span").text(`Top: ${offset.top}px, Left: ${offset.left}px`);
+
+    const box2 = $("#box2");
+    // TODO: 幅設定
+    // TODO: 高さ設定
+    // TODO: 位置設定
+});
