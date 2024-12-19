bafc4cdae73a53072a6b350accf63679131748d1
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-19 15:42:34 +0900

    update

--- 25_jquery_menu/js/slide2.js
+++ 25_jquery_menu/js/slide2.js
@@ -2,27 +2,25 @@ $(function () {
     const $menuButton = $('#menu-button');
     const $menuLink = $('#slide-menu a[href^="#"]');
     const $slideMenu = $('#slide-menu');
-    const headerHeight = $('#header-menu').outerHeight() + 10;
+    const headerHeight = $('#header-menu').outerHeight(); // ヘッダーの高さを取得
 
     // 初期化処理
     function initMenu() {
-        // TODO: css指定：left: -100%
         $slideMenu.css({
-            position: 'absolute', // メニューを絶対位置に設定
-            left: '-100%' // メニューを画面外に隠す
+            position: 'fixed', // 固定位置
+            top: headerHeight + 'px', // ヘッダーの下に配置
+            left: '-100%',    // 初期状態は画面外に隠す
+            height: `calc(100vh - ${headerHeight}px)` // ヘッダー分の高さを差し引く
         });
     }
 
     // スライド処理
     function slideMenu() {
         let x = '0px';
-        // TODO: スライドメニューの class=on をトグル
         $slideMenu.toggleClass('on');
         if (!$slideMenu.hasClass('on')) {
-            // TODO: x をスライドメニューの幅分左に移動
             x = '-' + $slideMenu.width() + 'px';
         }
-        // TODO: left に xを設定してアニメーション
         $slideMenu.animate({ left: x }, 300);
     }
 
