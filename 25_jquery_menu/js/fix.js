0bededd59012f5dea4e0481b8f2413b145ab5dfa
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-19 16:12:17 +0900

    update

--- 25_jquery_menu/js/fix.js
+++ 25_jquery_menu/js/fix.js
@@ -1,9 +1,12 @@
 // TODO: id=nav のY座標取得: offset().top
 const navPos = $('#nav').offset().top
 $(window).scroll(function () {
-    console.log($(window).scrollTop())
+    // console.log($(window).scrollTop())
     if ($(window).scrollTop() > navPos) {
         // TODO: 固定 css設定：position: fixed
+        $('#nav').css({
+            position: 'fixed',
+        });
     } else {
         // TODO: 固定解除 css設定：position: static
     }
